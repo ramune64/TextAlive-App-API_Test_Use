e49050a7babec1ea0dbcc9545d2b02b981e3559f
ramune64 <ramune69atrrsc@gmail.com>
2024-05-20 23:24:12 +0900

    20240520

--- js/script.js
+++ js/script.js
@@ -24,6 +24,10 @@ const text_get_line = (linedata,currentChord) => {
   return text_line;
 }
 
+function map_value(x, in_min, in_max, out_min, out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 const player = new Player({
   // トークンは https://developer.textalive.jp/profile で取得したものを使う
   app: { token: "lk6HkxIEwJqCRJis" },
@@ -38,6 +42,10 @@ const player = new Player({
 
 let previousLine = null;  // 前回の行情報を保持する変数
 let previousStartTime = null;  // 前回の行の発声開始時間を保持する変数
+let line_counter = 0;
+const ryli = document.getElementById("ryli");
+const ryli_n = document.getElementById("ryli_n");
+
 
 
 player.addListener(
@@ -92,14 +100,13 @@ player.addListener(
 
   onTimerReady() {
     document.querySelector("#word").textContent = "準備完了";
-    document.querySelector("#ryli").textContent = "";
   },
   onPlay() {
     let w = player.video.firstWord;
   },
   onTimeUpdate: (position) => {
     // 現在の再生位置に対応する歌詞の行を取得
-    const currentChar = player.video.findChar(position+200);//現在の200ms先を取得
+    const currentChar = player.video.findChar(position+300);//現在の300ms先を取得
     const beat = player.findBeat(position+200);
     /* console.log(player.data.songMap.chords); */
     const chords = player.data.songMap.chords;
@@ -108,17 +115,33 @@ player.addListener(
       /* console.log("Current chord:", currentChord.name);
       console.log(player.getValenceArousal(player.timer.position)["v"]);
       console.log(player.getVocalAmplitude(player.timer.position)); */
-      
-      document.querySelector("#kanzyou").textContent = player.getValenceArousal(player.timer.position)["v"];
+      //document.querySelector("#kanzyou").textContent = player.getValenceArousal(player.timer.position)["v"];
       document.querySelector("#code").textContent = currentChord.name;
-      if(currentChord.name[0] == "F"){
-        color = "pink";
+      if(currentChord.name[0] == "A"){
+        color = "rgb(255, 255, 0)";
+      }else if(currentChord.name[0] == "B"){
+        color = "rgb(255, 250, 0)";
+      }else if(currentChord.name[0] == "C"){
+        color = "rgb(255, 245, 0)";
+      }else if(currentChord.name[0] == "D"){
+        color = "rgb(255, 240, 0)";
+      }else if(currentChord.name[0] == "E"){
+        color = "rgb(255, 235, 0)";
+      }else if(currentChord.name[0] == "F"){
+        color = "rgb(255, 230, 0)";
+      }else if(currentChord.name[0] == "G"){
+        color = "rgb(255, 225, 0)";
       }else{
-        color = "yellow";
       }
-      long = (((player.getValenceArousal(player.timer.position)["v"]*100)/65)*100) + "%";
-      console.log(`long: ${(player.getValenceArousal(player.timer.position)["v"]*100)/65*100}`)
-      document.getElementById("section").style.background = `linear-gradient(white  ${long},${color})`
+      //console.log(player.getValenceArousal(player.timer.position));
+      //long = (100-(((player.getValenceArousal(player.timer.position)["a"]*100)-30)*10)+0) + "%";
+      //long =100- map_value(player.getValenceArousal(player.timer.position)["v"]*100-22,0,10,0,100);
+      long =100- (player.getValenceArousal(player.timer.position)["v"] + player.getValenceArousal(player.timer.position)["a"])*100
+      /* if(player.findChorus(player.timer.position) !== null){
+        long -= 10;
+      } */
+      console.log(`long: ${long}`)
+      document.getElementById("section").style.background = `linear-gradient(rgba(0,0,0,0) ${long}%,${color})`
     }
     /* if (notes) {
       const currentNotes = notes.filter(note => note.startTime <= position && note.endTime >= position);
@@ -133,13 +156,33 @@ player.addListener(
       const currentLine = currentChar.parent.parent;
       const currentStartTime = currentLine.startTime;
       if(currentLine !== previousLine && currentStartTime !== previousStartTime){
-        document.querySelector("#onryou").textContent = player.getVocalAmplitude(player.timer.position);
+        //document.querySelector("#onryou").textContent = player.getVocalAmplitude(player.timer.position);
         console.log(currentChar);//データはここにある
         now_line = text_get_line(currentLine);//発声中の行の歌詞取得
         next_line = text_get_line(currentLine.next);//次のぎょうの歌詞取得
         console.log(`Current line: ${now_line}`);//発声中の行の歌詞表示(コンソール)
-        document.querySelector("#ryli").textContent = now_line;//発声中の行の歌詞表示
-        document.querySelector("#ryli_n").textContent = next_line;//次の行の歌詞表示
+
+        line_counter += 1;
+        if(line_counter%2 == 1){
+          document.querySelector("#ryli").textContent = now_line;//発声中の行の歌詞表示
+          ryli.classList.remove("rout");
+          ryli_n.classList.remove("rin");
+
+          ryli.classList.add("rin");
+          ryli_n.classList.add("rout");
+          //console.log("%1");
+        }else{
+          document.querySelector("#ryli_n").textContent = now_line;//発声中の行の歌詞表示
+          ryli.classList.remove("rin");
+          ryli_n.classList.remove("rout");
+
+          ryli.classList.add("rout");
+          ryli_n.classList.add("rin");
+          //console.log("%0");
+        }
+
+       /*  document.querySelector("#ryli").textContent = now_line;//発声中の行の歌詞表示
+        document.querySelector("#ryli_n").textContent = next_line;//次の行の歌詞表示 */
         previousLine = currentLine;  // 前回の行情報を更新
         previousStartTime = currentStartTime;  // 前回の発声開始時間を更新
 
